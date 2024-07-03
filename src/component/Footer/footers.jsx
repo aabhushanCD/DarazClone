b82bf14047bfadeb3120aadde876c2eae7e2d980
aabhushanCD <dammardhakal426@gmail.com>
2024-07-03 21:34:00 +0545

    footer and jquery

---
+++ src/component/Footer/footers.jsx
@@ -0,0 +1,16 @@
+import Grid from '@mui/material/Grid'
+function Footers() {
+  return (
+    <>
+      <Grid container xs="12" spacing={10} >
+        <Grid item  >kdsjfkj</Grid>
+        <Grid item >kdjfkdasjflkjdsafsaldflkds</Grid>
+        <Grid item >dd</Grid>
+        <Grid item >sd</Grid>
+
+      </Grid>
+    </>
+  );
+}
+
+export default Footers;
